f4ab757297ceb3eb3ff067f304e133523ad30136
pahiina87 <156676648+pahiina87@users.noreply.github.com>
2024-09-17 12:43:19 -0600

    added getComputerChoice

---
+++ rps.js
@@ -0,0 +1,7 @@
+let choice = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice(choice) {
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+console.log(getComputerChoice(choice));
